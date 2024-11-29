4915d78d9945c4b14f50484d809b59c8bbbe19da
jotajuani <juanijimenez809@gmail.com>
2024-11-29 13:05:02 -0300

    decouple added

--- portfolioweb/portfolioweb/settings.py
+++ portfolioweb/portfolioweb/settings.py
@@ -9,9 +9,11 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
+from decouple import config
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+
 
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -22,12 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
-# ALLOWED_HOSTS = ['portfolioweb.herokuapp.com']
 ALLOWED_HOSTS = ['127.0.0.1', 'juanjimenez.com', 'www.juanjimenez.com']
 ALLOWED_HOST_ENV = os.environ.get('ALLOWED_HOSTS')
 if ALLOWED_HOST_ENV:
@@ -142,9 +143,10 @@ MEDIA_ROOT = '/vol/web/media'
 # MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
 MEDIA_URL = '/static/media/'
 
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
